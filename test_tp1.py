5975e490fb54ddfce292683d64450b9957866092
rdossou <dossouroland@hotmail.fr>
2022-08-06 13:59:49 +0200

    use tp1test

--- test_tp1.py
+++ test_tp1.py
@@ -31,48 +31,4 @@ def test_carrefour():
     # Close cookies pop up
     close_cookies = wait.until(expected_conditions.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
     close_cookies.click()
-
-def test_page_object():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://www.carrefour.fr")
-
-    home = HomePage(driver)
-    home.closeCookie()
-    home.OpenMenu()
-    home.OpenEpicerieSalee()
-    home.Open_Sub_category()
-    home.Open_Product_category()
-
-    Produit = ProductCategoryPage(driver)
-    Produit.OpenProductPage(3)
-
-    Acheter = ProductPage(driver)
-    Acheter.Buy()
-    Acheter.Choose_Delivery_Method()
-    Acheter.Enter_Zip_Code()
-    Acheter.Select_First_Store()
-
-    Message_attendu = "1 produit indisponible dans ce magasin."
-    assert Acheter.Get_Availability_Status() == Message_attendu
-
-
-
-
-
-
-
-
-
-def test_open_chrome():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://www.amazon.fr")
-    barre_recherche = driver.find_element(By.CSS_SELECTOR,"['#twotabsearchtextbox'")
-    time.sleep(2)
-    barre_recherche.send_keys("Playsattion 5" + Keys.ENTER)
-    driver.quit()
-
-
-
-
+    
